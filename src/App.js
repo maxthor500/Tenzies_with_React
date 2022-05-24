c387341711faf4c64ff38900db3bde988729d5d3
maxthor500 <iraubuntu@gmail.com>
2022-05-24 14:41:13 +0100

    Add a function to hold the dice and update the rollDice avoiding to roll the held ones

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
   //  Initialize the state by calling our `allNewDice` function
   const [dice, setDice] = React.useState(allNewDice())
 
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6), 
+      isHeld: false,
+      id: nanoid(),
+    }
+  }
+
   /**
    * Write a function (allNewDice) that returns an array 
    * of 10 random numbers between 1-6 inclusive.
@@ -17,25 +25,30 @@ function App() {
 
     //loop 10 times
     for(let i=0; i<10; i++) {
-      // generate an Dice object
-      const diceObject = {
-        value: Math.ceil(Math.random() * 6), 
-        isHeld: false,
-        id: nanoid(),
-      }
-      // push the object in the array
-      diceValues.push(diceObject)
+      // push the new die in the array
+      diceValues.push(generateNewDie())
     }
     return diceValues
   }
 
   function rollDice() {
-    setDice(allNewDice)
+    setDice(prevDice => prevDice.map(die => {
+        return die.isHeld ? die : generateNewDie()
+      })
+    )
   }
 
   function holdDice(id) {
-    console.log(id)
+    // change the value of die.isHeld when the die is clicked
+    setDice(prevDice => {
+      return prevDice.map((die) => {
+        return die.id === id ? {
+          ...die, 
+          isHeld: !die.isHeld} : die
+      })}
+    )
   }
+  
   /**
    * Map over the state numbers array to generate our array
    * of Die elements and render those in place of our
