3f6460bb2a79c2574bc5dbca344fc3806fe8e3bd
maxthor500 <iraubuntu@gmail.com>
2022-05-25 10:58:57 +0100

    Style the dice replacing the numbers with dots

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,24 +1,15 @@
-/**
- * Challenge 1:
- * 
- * - Create a Die component that takes a `value` prop
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components 
- *   to look like they do in the slide
- */
+import "../dice.css"
 
 export default function Die(props) {
     const styleHeldDie = {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
+
     return (
         <div 
-            className="die-face" 
+            className={`die-face dice dice-${props.value}`} 
             style={styleHeldDie}
             onClick={props.holdDice}>
-            <h2 className="text-die" >{props.value}</h2>
         </div>
     )
 }
